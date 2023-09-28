fc086b64ec558dd638d113f3e700d684c7d84c9d
Osar <oscar_mcintosh@msn.com>
2023-09-28 10:22:56 -0500

    added date selector

---
+++ components/DataSelector.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <label for="selectedDate">Select a Date:</label>
+    <input
+      type="date"
+      id="selectedDate"
+      v-model="selectedDate"
+    />
+    <p>Selected Date: {{ selectedDate }}</p>
+  </div>
+</template>
+
+<script setup>
+const selectedDate = ref(null);
+</script>
